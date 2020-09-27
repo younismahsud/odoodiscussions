6d3384bd1f0def53b151d1c022efec79e4a166d4
Younis <m.younis.mahsud@gmail.com>
2020-09-27 11:07:49 -0700

    Lecture 26

--- openacademy/models/openacademy.py
+++ openacademy/models/openacademy.py
@@ -23,5 +23,13 @@ class Session(models.Model):
     country_id = fields.Many2one('res.country', related='instructor_id.country_id')
     course_id = fields.Many2one('openacademy.course', ondelete='cascade', string="Course", required=True)
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
+    taken_seats = fields.Float(string="Taken seats", compute='_taken_seats')
 
+    @api.depends('seats', 'attendee_ids')
+    def _taken_seats(self):
+        for r in self:
+            if not r.seats:
+                r.taken_seats = 0.0
+            else:
+                r.taken_seats = 100.0 * len(r.attendee_ids) / r.seats
 
