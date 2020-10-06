50c148e29694c955f4be7a2e20059f091704b88c
Younis <m.younis.mahsud@gmail.com>
2020-10-06 00:47:56 -0700

    On Change decorator

--- openacademy/models/openacademy.py
+++ openacademy/models/openacademy.py
@@ -34,3 +34,20 @@ class Session(models.Model):
             else:
                 r.taken_seats = 100.0 * len(r.attendee_ids) / r.seats
 
+    @api.onchange('seats', 'attendee_ids')
+    def _verify_valid_seats(self):
+        if self.seats < 0:
+            return {
+                'warning': {
+                    'title': "Incorrect 'seats' value",
+                    'message': "The number of available seats may not be negative",
+                },
+            }
+        if self.seats < len(self.attendee_ids):
+            return {
+                'warning': {
+                    'title': "Too many attendees",
+                    'message': "Increase seats or remove excess attendees",
+                },
+            }
+
