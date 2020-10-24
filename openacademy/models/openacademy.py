20c5a1fa07e6a08fd75e8407e34c3b2879eba884
Younis <m.younis.mahsud@gmail.com>
2020-10-23 23:51:53 -0700

    Advanced tree view

--- openacademy/models/openacademy.py
+++ openacademy/models/openacademy.py
@@ -1,5 +1,6 @@
 from odoo import models, fields, api
 from odoo.exceptions import ValidationError
+from datetime import timedelta
 
 
 class Course(models.Model):
@@ -24,7 +25,6 @@ class Course(models.Model):
         default['course_name'] = new_name
         return super(Course, self).copy(default)
 
-
     _sql_constraints = [
         ('name_description_check',
          'check (course_name != description)',
@@ -43,6 +43,7 @@ class Session(models.Model):
     name = fields.Char(required=True)
     start_date = fields.Date(default=fields.date.today())
     duration = fields.Float(digits=(6, 2), help="Duration in days")
+    end_date = fields.Date(string="End Date", store=True, compute='_get_end_date', inverse='_set_end_date')
     seats = fields.Integer(string="Number of seats")
     instructor_id = fields.Many2one('res.partner', string="Instructor", domain=[('country_id', '=', 'Belgium')])
     country_id = fields.Many2one('res.country', related='instructor_id.country_id')
@@ -50,6 +51,27 @@ class Session(models.Model):
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
     taken_seats = fields.Float(string="Taken seats", compute='_taken_seats')
     active = fields.Boolean(string='Active', default=True)
+
+    @api.depends('start_date', 'duration')
+    def _get_end_date(self):
+        for r in self:
+            if not (r.start_date and r.duration):
+                r.end_date = r.start_date
+                continue
+
+            # Add duration to start_date, but: Monday + 5 days = Saturday, so
+            # subtract one second to get on Friday instead
+            duration = timedelta(days=r.duration, seconds=-1)
+            r.end_date = r.start_date + duration
+
+    def _set_end_date(self):
+        for r in self:
+            if not (r.start_date and r.end_date):
+                continue
+
+            # Compute the difference between dates, but: Friday - Monday = 4 days,
+            # so add one day to get 5 days instead
+            r.duration = (r.end_date - r.start_date).days + 1
 
     @api.constrains('instructor_id', 'attendee_ids')
     def _check_instructor_not_in_attendees(self):
